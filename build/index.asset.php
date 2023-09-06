8b182f0a15bc5ba51ff195321fdcd752aff96ef7
harshal <kardileharshal567@gmail.com>
2023-09-06 19:43:13 +0530

    commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0a2bff230483e572c668');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '69ff95521d9d8311f91f');
---
+++ text-box.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       Text box
+ * Description:       A box of text.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Harshalk
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       text-box
+ *
+ * @package           blocks-course
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function blocks_course_text_box_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'blocks_course_text_box_block_init' );
